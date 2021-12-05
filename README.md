7181f38f94e4c38943935e7a795b97adf1d7a55f
NedaParvini <nedaparvini1@gmail.com>
2021-12-04 19:15:25 -0800

    Bold the text

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# Title:README FILE GENERATOR
-          # Description:This codes generate Readme file 
-          # Table of Content:undefined
-          # GitHub:(https://github.com/NedaParvini)
-          # Repository:(https://github.com/NedaParvini/ReadMeGeneratoe)
-          # Installation instructions:N/A
-          # Usage information:N/A
-          # Contribution guidelines:N/A\
-          # Testinstructions:N/A
+# Title:ReadMe File Generator
+          **Description**This VS codes generates READ ME FILES
+          **Table of Content**undefined
+          # GitHub(https://github.com/NedaParvini)
+          # Repository(https://github.com/NedaParvini/ReadMeGenerator)
+          # Installation instructionsN/A
+          # Usage informationN/A
+          # Contribution guidelinesN/A
+          # TestinstructionsN/A
           The license is Apache
           ![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)  
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -21,14 +21,14 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# Title:${data.title}
-          # Description:${data.Description}
-          # Table of Content:${(data.TableofContent)}
-          # GitHub:(https://github.com/${data.github})
-          # Repository:(https://github.com/${data.github}/${data.repo})
-          # Installation instructions:${data.installation}
-          # Usage information:${data.usageinformation}
-          # Contribution guidelines:${data.contributionguidelines}
-          # Testinstructions:${data.testinstructions}
+          **Description**${data.Description}
+          **Table of Content**${(data.TableofContent)}
+          # GitHub(https://github.com/${data.github})
+          # Repository(https://github.com/${data.github}/${data.repo})
+          # Installation instructions${data.installation}
+          # Usage information${data.usageinformation}
+          # Contribution guidelines${data.contributionguidelines}
+          # Testinstructions${data.testinstructions}
           ${renderLicenseBadge(data.license)}
           ![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)  
 `;
