a66d29eafc88ccee489eb871a55faa00980e3e67
NedaParvini <nedaparvini1@gmail.com>
2021-12-05 13:28:26 -0800

    Both Files has been changed

--- index.js
+++ index.js
@@ -1,64 +1,133 @@
-const util = require('util');
-const generateMarkdown= require ('./utils/generateMarkdown'); 
+
+
 // TODO: Include packages needed for this application
-const inquirer = require ("inquirer");
-const fs = require ("fs");
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+const emailValidator = require('email-validator');
+
+const generateMarkdown = require('./utils/generateMarkdown');
 
 
 
 // TODO: Create an array of questions for user input
 const questions = [
-    {type:"input", name:"title", message:"What Is Your Project Title?", choices:"listtype" },
-    {type:"input", name:"Description", message:"What Is Your Project Description?", choices:"listtype" },
-    {type:"checkbox", name:"TableofContents", message:"Select a Table of Contents", choices:["Installation instructions","Usage information","Contribution guidelines","Testinstructions","License"]}, 
-    {type:"list", name:"license", message:"Choose a liscence", choices:["MIT","Apache","GPL"] },
-    {type:"input",name: "github", message: "What is your GitHub Username?", validate: (value) => {
-            if (value) { return true } else { return 'Please enter your username.' }},},
-            {type:"input",name: "repo", message: "What is your GitHub Repository For This Project?", validate: (value) => {
-                if (value) { return true } else { return 'Please enter your Repo.' }},},
-    {type:"input", name:"installation", message:"Please Enter installation instructions", choices:"listtype" },
-    {type:"input", name:"usageinformation", message:"Please Enter Usage Information", choices:"listtype" },
-    {type:"input", name:"contributionguidelines", message:"Please Enter Contribution guidelines", choices:"listtype" },
-    {type:"input", name:"testinstructions", message:"Please Enter Test Instructions", choices:"listtype" },
-    
-    
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is your project title?',
+    validate: (value) => {
+        if (value) { return true } else { return 'Please enter your project title.' }
+    },
+},
+{
+  type: 'input',
+  name: 'description',
+  message: 'What is the description of your project?',
+  validate: (value) => {
+      if (value) { return true } else { return 'Please enter description.' }
+  },
+},
+  { type: "list",
+   name: "license",
+   message: "Choose a liscence", 
+   choices: ["MIT", "Apache", "GPL"]
+   },
+   { type: "input",
+     name: "github", 
+     message: "What is your GitHub Username?", 
+     validate: (value) => {
+      if (value) { return true } else { return 'Please enter your username.' }
+    },
+  },
+   { type: "input",
+     name: "repo", 
+     message: "What is your GitHub Repository For This Project?",
+     validate: (value) => {
+      if (value) { return true } else { return 'Please enter your Repo.' }
+    }, 
+   },
+  { type: "input",
+    name: "installation",
+    message: "Please Enter installation instructions",
+    choices: "listtype",
+    validate: (value) => {
+      if (value) { return true } else { return 'Please Enter installation instructions' }
+    }, 
+  },
+  { type: "input",
+    name: "usageinformation",
+    message: "Please Enter Usage Information",
+    choices: "listtype",
+    validate: (value) => {
+      if (value) { return true } else { return 'Please Enter Usage Information' }
+    }, 
+  },
+  { type: "input",
+    name: "contributionguidelines",
+    message: "Please Enter Contribution guidelines",
+    choices: "listtype" ,
+    validate: (value) => {
+      if (value) { return true } else { return 'Please Enter Contribution guidelines' }
+    }, 
+  },
+  { type: "input",
+    name: "testinstructions",
+    message: "Please Enter Test Instructions",
+    choices: "listtype",
+    validate: (value) => {
+      if (value) { return true } else { return 'Please Enter Test Instructions' }
+    }, 
+   },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?',
+    validate: (value) => {
+        if (emailValidator.validate(value)) {
+            return true
+        } else { return 'Please enter a valid email address' }
+    },
+}
+
+
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    console.log(fileName, data)
-    fs.writeFile(fileName, data, (err) => {
-        if (err)
-          console.log(err);
-        else {
-          console.log("File written successfully\n");
-          console.log("The written has the following contents:");
-          console.log(fs.readFileSync(fileName, "utf8"));
-        }
-      });
+  console.log(fileName, data)
+  fs.writeFile(fileName, data, (err) => {
+    if (err)
+      console.log(err);
+    else {
+      console.log("File written successfully\n");
+      console.log("The written has the following contents:");
+      console.log(fs.readFileSync(fileName, "utf8"));
+    }
+  });
 
 }
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer
-  .prompt(questions
-    /* Pass your questions in here */
-  )
-  .then((answers) => {
+  inquirer
+    .prompt(questions
+      /* Pass your questions in here */
+    )
+    .then((answers) => {
       console.log(answers)
-    // Use user feedback for... whatever!!
-    var generate = generateMarkdown(answers);
-    console.log(generate);
-    writeToFile("README.md", generateMarkdown(data));
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
+      // Use user feedback for... whatever!!
+      var generate = generateMarkdown(answers);
+      console.log(generate);
+      writeToFile("README.md", generateMarkdown(data));
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+      } else {
+        // Something else went wrong
+      }
+    });
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,3 +1,33 @@
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# Title:${data.title}
+          **Description**
+          ${data.description}
+          ## Table of Contents
+          * [Installation instructions](#Installation instructions)
+          * [Usage information](#Usage information)
+          * [Contribution guidelines](#Contribution guidelines)
+          * Testinstructions](#Testinstructions)
+          * [License](#license)
+          ##Installation instructions
+          ${data.installation}
+          ##Usage information
+          ${data.usageinformation}
+          ##Contribution guidelines
+          ${data.contributionguidelines}
+          ##Testinstructions
+          ${data.testinstructions}
+          ##License
+          ${renderLicenseBadge(data.license)}
+          ![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)  
+          Any additional questions or feed back, feel free to [send an email](mailto:${data.email}). ## License
+          **GitHub**
+          (https://github.com/${data.github})
+          **Repository**
+          (https://github.com/${data.github}/${data.repo})
+          
+`;
+}
 // TODO: Create a function that returns a license badge based on which license is passed in
 
 const CheckboxPrompt = require("inquirer/lib/prompts/checkbox");
@@ -18,35 +48,7 @@ function renderLicenseSection(license) {}
 
 
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# Title:${data.title}
-          **Description**
-          ${data.Description}
-          **Table of Content**
-          ${(data.TableofContent)}
-          * [Installation instructions](#Installation instructions)
-          * [Usage information](#Usage information)
-          * [Contribution guidelines](#Contribution guidelines)
-          * Testinstructions](#Testinstructions)
-          * [License](#license)
-          **GitHub**
-          (https://github.com/${data.github})
-          **Repository**
-          (https://github.com/${data.github}/${data.repo})
-          ##Installation instructions
-          ${data.installation}
-          ##Usage information
-          ${data.usageinformation}
-          ##Contribution guidelines
-          ${data.contributionguidelines}
-          ##Testinstructions
-          ${data.testinstructions}
-          ##License
-          ${renderLicenseBadge(data.license)}
-          ![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)  
-`;
-}
+
 
 module.exports = generateMarkdown;
 
